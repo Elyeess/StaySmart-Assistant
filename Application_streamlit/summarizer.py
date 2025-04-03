1f717a46dc2b7adcd0d65d3f3616afe386ec76f1
doniatekaya <166394585+doniatekaya@users.noreply.github.com>
2025-04-03 21:44:06 +0100

    Delete Application_streamlit/summarizer.py

--- Application_streamlit/summarizer.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small")
-
-def summarize_text(text):
-    try:
-        return summarizer(text, max_length=100, min_length=30, do_sample=False)[0]['summary_text']
-    except:
-        return "Résumé non disponible."
